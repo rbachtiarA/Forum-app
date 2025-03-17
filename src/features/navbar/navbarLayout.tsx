be5299c25e4fba1af15ecbee5f4a1ed3e4ad9202
rbachtiarA <rbachtiarabdalla@gmail.com>
2025-03-17 10:09:16 +0700

    chore: add env template

--- src/features/navbar/navbarLayout.tsx
+++ src/features/navbar/navbarLayout.tsx
@@ -6,8 +6,8 @@ export default function NavbarLayout() {
     <nav className="w-full flex flex-row-reverse md:flex-row p-1 justify-between bg-secondary text-foreground items-center h-[var(--navbar-height)]">
       <ol className="md:flex flex-row w-full gap-2 hidden">
         <Link href={'/'}>Home</Link>
-        <li>Products</li>
-        <li>Sales</li>
+        <li>Search</li>
+        <li>My Posts</li>
         <li>Settings</li>
       </ol>
       <div className="flex gap-x-1 h-full">
