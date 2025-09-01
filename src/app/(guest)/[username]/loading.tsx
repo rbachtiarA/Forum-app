43ed9347cd9a73f02897331241bec8ceafff3612
rbachtiarA <rbachtiarabdalla@gmail.com>
2025-09-01 15:49:28 +0700

    chore: add loading for profile and other prettier format

---
+++ src/app/(guest)/[username]/loading.tsx
@@ -0,0 +1,7 @@
+export default async function loading() {
+  return (
+    <div>
+      <h1>LOADING ....</h1>
+    </div>
+  );
+}
--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -4,32 +4,37 @@ import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-    const searchParams = request.nextUrl.searchParams
-    const query = searchParams.get('code')
+  try {
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get("code");
 
-    if(!query) redirect('/sign-in')
-    const supabase = await createServerSideClient()
-    const { data, error } = await supabase.auth.exchangeCodeForSession(query)
-    if(data.user) {
-        const userProfile = await prisma.profile.findUnique({
-            where: {
-                id: data.user.id
-            }
-        })
+    if (!query) redirect("/sign-in");
+    const supabase = await createServerSideClient();
 
-        if(!userProfile) {
-            await prisma.profile.create({
-                data: {
-                    id: data.user.id,
-                    username: data.user.email!.split('@')[0],
-                    picture: ''
-                }
-            })
-        }
-        return redirect('/')
+    const { data, error } = await supabase.auth.exchangeCodeForSession(query);
+    if (data.user) {
+      const userProfile = await prisma.profile.findUnique({
+        where: {
+          id: data.user.id,
+        },
+      });
+
+      if (!userProfile) {
+        await prisma.profile.create({
+          data: {
+            id: data.user.id,
+            username: data.user.email!.split("@")[0],
+            picture: "",
+          },
+        });
+      }
+      return redirect("/");
     }
 
-    if(error) {
-        return redirect('/sign-in')
+    if (error) {
+      throw error.cause;
     }
+  } catch {
+    return redirect("/sign-in");
+  }
 }
