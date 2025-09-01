8a4d6cbb8017b8ea169e5fce1170f8ea20d734b4
rbachtiarA <rbachtiarabdalla@gmail.com>
2025-09-01 15:50:40 +0700

    fix: unsaved change

--- src/features/feed/PostsFeed.tsx
+++ src/features/feed/PostsFeed.tsx
@@ -1,45 +1,49 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { useQuery } from '@tanstack/react-query'
-import { feedAPIOptions } from './feedQueries'
-import PostCard from './PostCard'
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { feedAPIOptions } from "./feedQueries";
+import PostCard from "./PostCard";
 
-export default function PostsFeed({username} :{ username?: string }) {
-  const currentDate = new Date()
-  const { data, isError, isLoading } = useQuery(feedAPIOptions(username))      
-    
-  if(isLoading) return (
-    <div>
-      Fetching Data...
-    </div> 
-  )
+export default function PostsFeed({ username }: { username?: string }) {
+  const currentDate = new Date();
+  const { data, isError, isLoading } = useQuery(feedAPIOptions(username));
 
-  if(isError || !data) return (
-    <div>
-      <h1>500 : Internal Server Error</h1>
-      <h2>Something is wrong, wait for our staff to fix it</h2>
-    </div>
-  )
+  if (isLoading) return <div>Fetching Data...</div>;
 
-  if(data?.posts.length === 0) return (
-    <div>
-      <h1>{username ? 'User has not post anything yet' : 'There is no post to show'}</h1>
-    </div>
-  )
+  if (isError || !data)
+    return (
+      <div>
+        <h1>500 : Internal Server Error</h1>
+        <h2>Something is wrong, wait for our staff to fix it</h2>
+      </div>
+    );
+
+  if (data?.posts.length === 0)
+    return (
+      <div>
+        <h1>
+          {username
+            ? "User has not post anything yet"
+            : "There is no post to show"}
+        </h1>
+      </div>
+    );
 
   return (
-    <>      
-      {
-        data &&
-        <div className='flex flex-col gap-y-2 mt-2'>
-          {
-            data.posts.map((post) => (
-                <PostCard key={post.id} post={post} currentDate={currentDate} user={data.users[post.user]}/> 
-            ))
-          }
+    <>
+      {data && (
+        <div className="flex flex-col gap-y-2 mt-2">
+          {data.posts.map((post) => (
+            <PostCard
+              key={post.id}
+              post={post}
+              currentDate={currentDate}
+              user={post.user}
+            />
+          ))}
         </div>
-      }
+      )}
     </>
-  )
+  );
 }
