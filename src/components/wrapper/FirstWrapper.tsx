59535ca34ebdbc8b453de242ae0a86c83ed9cc0a
rbachtiarA <rbachtiarabdalla@gmail.com>
2025-12-06 19:40:18 +0700

    fix: adjust padding and grid classes in FirstWrapper component

--- src/components/wrapper/FirstWrapper.tsx
+++ src/components/wrapper/FirstWrapper.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react";
 
 export default function FirstWrapper({ children }: { children: ReactNode }) {
   return (
-    <div className="w-full md:px-6 lg:px-10 xl:px-14 py-1 grid md:grid-cols-[1fr_4fr_1fr]">
+    <div className="w-full px-2 md:px-6 lg:px-10 xl:px-14 py-1 md:grid md:grid-cols-[1fr_4fr_1fr]">
       {children}
     </div>
   );
